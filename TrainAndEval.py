b93913ee46dc936286e7a3b413bc3fc0f61847b8
Hanlard <491377729@qq.com>
2020-03-08 17:28:19 +0800

    加入了使用[CLS]判断两个句子是否是QA关系的代码，提升模型综合效果

--- TrainAndEval.py
+++ TrainAndEval.py
@@ -12,21 +12,26 @@ import torch.optim as optim
 import warnings
 warnings.filterwarnings('ignore')
 from tqdm import tqdm
+from DocumentRetrieval import Knowledge
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 import random
+import jieba
 # 字符ID化
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--early_stop", type=int, default=5)
+parser.add_argument("--early_stop", type=int, default=1)
 parser.add_argument("--lr", type=float, default=1e-5)
 parser.add_argument("--l2", type=float, default=1e-5)
-parser.add_argument("--n_epochs", type=int, default=50)
+parser.add_argument("--n_epochs", type=int, default=5)
 parser.add_argument("--Negweight", type=float, default=0.01)
 parser.add_argument("--trainset", type=str, default="data/me_train.json")
 parser.add_argument("--devset", type=str, default="data/me_validation.ann.json")
 parser.add_argument("--testset", type=str, default="data/me_test.ann.json")
+parser.add_argument("--knowledge_path", type=str, default="data/me_test.ann.json")
+parser.add_argument("--Stopword_path",type=str, default= 'data/stop_words.txt')
 parser.add_argument("--device", type=str, default='cuda')
-parser.add_argument("--mode", type=str, default='train')  # eval / demo / train
+parser.add_argument("--mode", type=str, default='train')  # eval / demo / train / QA
 if os.name == "nt":
     parser.add_argument("--model_path", type=str, default="D:\创新院\智能问答\BERT for WebQA\save_model\latest_model.pt")
     parser.add_argument("--model_back", type=str, default="D:\创新院\智能问答\BERT for WebQA\save_model\\back_model.pt")
@@ -80,15 +85,17 @@ class WebQADataset(data.Dataset):
         tokens_id = tokenizer.convert_tokens_to_ids(tokens)
         token_type_ids = [0 if i <= tokens_id .index(102) else 1 for i in range(len(tokens_id ))]
         answer_offset = (-1, -1)
+        IsQA = 0
         answer_seq_label = len(tokens_id) * [0]
         if a != 'no_answer':
             answer_tokens = tokenizer.tokenize(a)
             answer_offset = self.FindOffset(tokens, answer_tokens)#有肯能返回False
             if answer_offset:#在原文中找到答案
                 answer_seq_label[answer_offset[0]:answer_offset[1]] = [1]*(len(answer_tokens))
+                IsQA = 1
             else:# self.FindOffset 返回False
                 answer_offset = (-1, -1)
-        return tokens, tokens_id, token_type_ids, answer_offset, answer_seq_label
+        return tokens, tokens_id, token_type_ids, answer_offset, answer_seq_label, IsQA
 
     def get_samples_weight(self,Negweight):
         samples_weight = []
@@ -100,7 +107,7 @@ class WebQADataset(data.Dataset):
         return np.array(samples_weight)
 
 def pad(batch):
-    tokens_l, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l= list(map(list, zip(*batch)))
+    tokens_l, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l= list(map(list, zip(*batch)))
     maxlen = np.array([len(sen) for sen in tokens_l]).max()
     ### pad和截断
     for i in range(len(tokens_l)):
@@ -113,7 +120,7 @@ def pad(batch):
         token_type_ids_l[i] = token_type_ids + (maxlen - len(tokens))*[1]
         tokens_id_l[i] =tokens_id + (maxlen - len(tokens))*tokenizer.convert_tokens_to_ids(['[PAD]'])
         answer_seq_label_l[i] = answer_seq_label + [0]*(maxlen - len(tokens))
-    return tokens_l, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l
+    return tokens_l, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l
 
 def result_metric(prediction_all, y_2d_all):
     total_num=0
@@ -127,27 +134,45 @@ def result_metric(prediction_all, y_2d_all):
 
 def TrainOneEpoch(model, train_iter, dev_iter, test_iter, optimizer, hp):
     model.train()
-    prediction_all, y_2d_all = [], []
+    CRFprediction_all, CRFloss_all, IsQAloss_all, y_CRF_all, IsQA_prediction_all, y_IsQA_all= [],[],[],[],[],[]
 
     best_acc = 0
     for i, batch in enumerate(tqdm(train_iter)):
-        _, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l = batch
+        _, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l = batch
         optimizer.zero_grad()
-        prediction, loss, y_2d  = model.module.forward(tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l)
-        prediction_all.append(prediction)
-        y_2d_all.append(y_2d)
+        IsQA_prediction, CRFprediction, IsQA_loss, crf_loss, y_2d, y_IsQA_2d  = model.module.forward(tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l)
+        ## CRF
+        CRFprediction_all.append(CRFprediction)
+        y_CRF_all.append(y_2d)
+
+        ## IsQA
+        IsQA_prediction_all.append(IsQA_prediction)
+
+        y_IsQA_all.append(y_IsQA_2d)
+
+        # loss
+        CRFloss_all.append(crf_loss.to("cpu").item())
+        IsQAloss_all.append(IsQA_loss.to("cpu").item())
+        loss = IsQA_loss + crf_loss
+
         # nn.utils.clip_grad_norm_(model.parameters(), 3.0)#设置梯度截断阈值
         loss.backward()## 计算梯度
         optimizer.step()## 根据计算的梯度更新网络参数
+
+
         if i % 100 == 0 and i > 0:
-            acc = result_metric(prediction_all, y_2d_all)
-            print("<Last 100 Steps MeanValue> Setp-{} Loss:{:.3f} acc:{:.3f}".format(i,loss.item(),acc))
-            prediction_all, y_2d_all = [], []
+            accCRF = result_metric(CRFprediction_all, y_CRF_all)
+            accIsQA = result_metric(IsQA_prediction_all, y_IsQA_all)
+
+            print("<Last 100 Steps MeanValue> Setp-{} IsQA-Loss: {:.3f} CRF-Loss: {:.3f}  "
+                  "CRF-Result: accCRF = {:.3f}  IsQA-Result: accIsQA = {:.3f}"
+                  .format(i,np.mean(IsQAloss_all), np.mean(CRFloss_all),accCRF,accIsQA))
+
         if i % 1000 == 0:
             print("Eval on Devset...")
-            dev_acc = Eval(model, dev_iter)
-            if dev_acc > best_acc:
-                best_acc = dev_acc
+            accIsQA, accCRF = Eval(model, dev_iter)
+            if accIsQA * accCRF > best_acc:
+                best_acc = accIsQA * accCRF
                 if i>1000:
                     print("Devdata 精度提升 备份模型至{}".format(hp.model_back))
                     torch.save(model, hp.model_back)
@@ -157,30 +182,91 @@ def TrainOneEpoch(model, train_iter, dev_iter, test_iter, optimizer, hp):
 def Eval(model, iterator):
 
     model.eval()
-    prediction_all, crf_loss_all, y_2d_all = [],[],[]
+    CRFprediction_all, CRFloss_all, IsQAloss_all, y_CRF_all, IsQA_prediction_all, y_IsQA_all= [],[],[],[],[],[]
     for i, batch in enumerate(iterator):
-        _, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l = batch
-        prediction, crf_loss, y_2d = model.module.forward(tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l)
-        prediction_all.append(prediction)
-        y_2d_all.append(y_2d)
-        crf_loss_all.append(crf_loss.to("cpu").item())
+        _, tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l = batch
+        IsQA_prediction, CRFprediction, IsQA_loss, crf_loss, y_2d, y_IsQA_2d  = model.module.forward(tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l,IsQA_l)
 
-    acc = result_metric(prediction_all, y_2d_all)
-    print("<本次评估结果> Eval-Loss: {:.3f}  Eval-Result: acc = {:.3f}".format(np.mean(crf_loss_all),acc))
-    return acc
+        ## CRF
+        CRFprediction_all.append(CRFprediction)
+        y_CRF_all.append(y_2d)
+
+        ## IsQA
+        IsQA_prediction_all.append(IsQA_prediction)
+        y_IsQA_all.append(y_IsQA_2d)
+
+
+        CRFloss_all.append(crf_loss.to("cpu").item())
+        IsQAloss_all.append(IsQA_loss.to("cpu").item())
+
+    accCRF = result_metric(CRFprediction_all, y_CRF_all)
+    accIsQA = result_metric(IsQA_prediction_all, y_IsQA_all)
+
+    print("<本次评估结果> IsQA-Loss: {:.3f} CRF-Loss: {:.3f} CRF-Result: accCRF = {:.3f} "
+          "IsQA-Result: accIsQA = {:.3f}".format(np.mean(IsQAloss_all), np.mean(CRFloss_all), accCRF, accIsQA))
+
+    return accIsQA, accCRF
 
 def Demo(model, q, e):
     tokens = tokenizer.tokenize('[CLS]' + q + '[SEP]' + e)  # list
     if len(tokens) > 512:
         tokens = tokens[:512]
-    tokens_id = [tokenizer.convert_tokens_to_ids(tokens)]
-    prediction = model.module.predict(tokens_id)
-    prediction = prediction.numpy()[0]
+    tokens_id = [tokenizer.convert_tokens_to_ids(tokens)]#[[101,...,102,...]]
+    token_type_ids = [0 if i <= tokens_id[0].index(102) else 1 for i in range(len(tokens_id))]
+    IsQA_prediction, CRFprediction = model.module.predict(tokens_id,token_type_ids)
+    CRFprediction = CRFprediction.numpy()[0]
+    IsQA_prediction = IsQA_prediction.numpy()[0]
     answer = ""
-    for i in range(len(tokens)):
-        if prediction[i].item()==1:
-            answer = answer + tokens[i]
+    if IsQA_prediction==1:
+        for i in range(len(tokens)):
+            if CRFprediction[i].item()==1:
+                answer = answer + tokens[i]
     return answer
+
+# def prepare_knowledge(knowledge_path,Stopword_path):
+#     def del_stopword(line, Stopword, ngram=False):
+#         line = list(jieba.cut(line))
+#         new = [word for word in line if word not in Stopword]
+#         if ngram:  # 返回2元语法
+#             N = len(line)
+#             for i, word_i in enumerate(line):
+#                 for j in range(min(i + 1, N - 1), N):
+#                     word_j = line[j]
+#                     if word_i not in Stopword and word_j not in Stopword:
+#                         new.append(word_i + ' ' + word_j)
+#         return new  # [w1,w2,...]
+#     print("正在准备知识库...")
+#     dataset = Knowledge(knowledge_path)
+#     with open(Stopword_path, "r", encoding="gbk") as f:
+#         Stopword = set(f.read().splitlines())
+#     documents = dataset.evidences
+#     corpus = [" ".join(del_stopword(e, Stopword)) for e in documents]
+#     vectorizer = CountVectorizer()  # ngram_range=(1,2)
+#     count = vectorizer.fit_transform(corpus)
+#     # 计算TF-IDF向量
+#     TFIDF = TfidfTransformer()
+#     tfidf_matrix = TFIDF.fit_transform(count)
+#     d_matrix = np.array(tfidf_matrix.toarray())
+#     vocabulary_ = vectorizer.vocabulary_
+#     return d_matrix, vocabulary_, del_stopword, Stopword, dataset
+#
+# def QA(model, question, xu, knowledge, q_list):
+#     xu.reverse()# 按相关度从大到小
+#     result = {}
+#     for index in xu:#[[1],[2],[3],...]
+#         q=question
+#         e=knowledge.evidences[index[0]]
+#
+#         answer=Demo(model,q,e)
+#         print("answer:",answer)
+#         print("evidence:",e)
+#         if answer in result:
+#             result[answer] = result[answer] + 1
+#         else:
+#             result[answer] = 1
+#     return result
+
+
 
 if __name__ == "__main__":
     print("="*20+" 超参 "+"="*20)
@@ -233,9 +319,9 @@ if __name__ == "__main__":
             os.makedirs(os.path.split(hp.model_path)[0])
 
         print("First Eval On TestData")
-        test_acc = Eval(model, test_iter)
+        accIsQA, accCRF = Eval(model, test_iter)
 
-        best_acc = max(0,test_acc )
+        best_acc = max(0, accIsQA*accCRF )
         no_gain_rc = 0#效果不增加代数
 
         for epoch in range(1, hp.n_epochs + 1):
@@ -246,11 +332,11 @@ if __name__ == "__main__":
             # dev_acc = eval(model, dev_iter)
 
             print(f"=========eval test at epoch={epoch}=========")
-            test_acc= eval(model, test_iter)
+            accIsQA, accCRF = Eval(model, test_iter)
 
-            if test_acc >best_acc:
-                print("精度值由 {:.3f} 更新至 {:.3f} ".format(best_acc, test_acc))
-                best_acc = test_acc
+            if accIsQA*accCRF >best_acc:
+                print("精度值由 {:.3f} 更新至 {:.3f} ".format(best_acc, accIsQA*accCRF))
+                best_acc = accIsQA*accCRF
                 print("=======保存模型=======")
                 torch.save(model, hp.model_path)
                 no_gain_rc = 0
@@ -283,9 +369,9 @@ if __name__ == "__main__":
             print('=======载入模型=======')
             model = torch.load(hp.model_path)
             print("Eval On TestData")
-            test_acc = Eval(model, test_iter)
+            _,_ = Eval(model, test_iter)
             print("Eval On DevData")
-            dev_acc = Eval(model, dev_iter)
+            _,_ = Eval(model, dev_iter)
         else:
             print("没有可用模型！")
 
@@ -318,6 +404,43 @@ if __name__ == "__main__":
                     break
         else:
             print("没有可用模型！")
+    # elif hp.mode =="QA":
+    #     if os.path.exists(hp.model_path):
+    #         print('=======载入模型=======')
+    #         model = torch.load(hp.model_path)
+    #         ques_num=1
+    #         #准备知识库
+    #         d_matrix, vocabulary_, del_stopword, Stopword, knowledge = prepare_knowledge(hp.knowledge_path,hp.Stopword_path)
+    #         while True:
+    #             # try:
+    #             print("请输入问题-{}:".format(ques_num))
+    #             question = input()
+    #             if question == "OVER":
+    #                 print("问答结束！")
+    #                 break
+    #             # 创建问句tf-idf向量
+    #             q_vector = np.zeros([1, d_matrix.shape[1]])
+    #             q_list = del_stopword(question,Stopword,ngram=False)
+    #             for word in q_list:
+    #                 if word in vocabulary_:
+    #                     q_vector[0,vocabulary_[word]] = 1.
+    #             dot = (np.mat(d_matrix))*(np.mat(q_vector.T))
+    #             xu=dot.argsort(0)[-5:].tolist()# [[12], [37], [10]] 最大的五个索引
+    #
+    #             start = time.time()
+    #             answer = QA(model, question, xu, knowledge, q_list)
+    #             end = time.time()
+    #
+    #             print("问题-{}的答案是:".format(ques_num))
+    #             print(answer)
+    #             print("耗时:{:.2f}毫秒".format((end-start)*1e3))
+    #
+    #             ques_num = ques_num + 1
+    #             # except:
+    #             #     print("问答结束！")
+    #             #     break
+    #     else:
+    #         print("没有可用模型！")
     else:
         print("--mode请选择：train/eval/demo, 请注意拼写")
 
--- model.py
+++ model.py
@@ -17,17 +17,22 @@ class Net(nn.Module):
 
         kwargs = dict({'target_size': self.labelnum, 'device': self.device})
         self.CRF = CRF(**kwargs)
+        self.CrossEntropyLoss = nn.CrossEntropyLoss()
+        self.fc2 = nn.Linear(self.hidden_size, 2, bias=True)
 
-    def forward(self,tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l):
+    def forward(self,tokens_id_l, token_type_ids_l, answer_offset_l, answer_seq_label_l, IsQA_l):
 
         ## 字符ID [batch_size, seq_length]
         tokens_x_2d = torch.LongTensor(tokens_id_l).to(self.device)
         token_type_ids_2d = torch.LongTensor(token_type_ids_l).to(self.device)
 
-        batch_size, seq_length = tokens_x_2d.size()
+        # 计算sql_len 不包含[CLS]
+        batch_size, seq_length = tokens_x_2d[:,1:].size()
 
-        ## 答案ID [batch_size, seq_length]
-        y_2d = torch.LongTensor(answer_seq_label_l).to(self.device)
+        ## CRF答案ID [batch_size, seq_length]
+        y_2d = torch.LongTensor(answer_seq_label_l).to(self.device)[:,1:]
+        ## (batch_size,)
+        y_IsQA_2d = torch.LongTensor(IsQA_l).to(self.device)
 
         if self.training: # self.training基层的外部类
             self.PreModel.train()
@@ -36,25 +41,49 @@ class Net(nn.Module):
             self.PreModel.eval()
             with torch.no_grad():
                 emb, _ = self.PreModel(input_ids=tokens_x_2d, token_type_ids=token_type_ids_2d)
+
+        ## [CLS] for IsQA  [batch_size, hidden_size]
+        cls_emb = emb[:,0,:]
+        ## [batch_size, 2]
+        IsQA_logits = self.fc2(cls_emb)
+        IsQA_loss = self.CrossEntropyLoss.forward(IsQA_logits,y_IsQA_2d)
+
+        ## [batch_size, 1]
+        IsQA_prediction = IsQA_logits.argmax(dim=-1).unsqueeze(dim=-1)
+
         # CRF mask
         mask = np.ones(shape=[batch_size, seq_length], dtype=np.uint8)
         mask = torch.ByteTensor(mask).to(self.device)
         # [batch_size, seq_len, 4]
-        crf_logits = self.CRF_fc1(emb)
-        crf_loss = self.CRF.neg_log_likelihood_loss(feats=crf_logits, mask=mask, tags=y_2d )
-        _, prediction = self.CRF.forward(feats=crf_logits, mask=mask)
-        return prediction, crf_loss, y_2d
 
-    def predict(self,tokens_id_l):
+        # No [CLS]
+        crf_logits = self.CRF_fc1(emb[:,1:,:])
+        crf_loss = self.CRF.neg_log_likelihood_loss(feats=crf_logits, mask=mask, tags=y_2d )
+        _, CRFprediction = self.CRF.forward(feats=crf_logits, mask=mask)
+
+        return IsQA_prediction, CRFprediction, IsQA_loss, crf_loss, y_2d, y_IsQA_2d.unsqueeze(dim=-1)# (batch_size,) -> (batch_size, 1)
+
+    def predict(self,tokens_id_l, token_type_ids_l):
         tokens_x_2d = torch.LongTensor(tokens_id_l).to(self.device)
-        batch_size, seq_length = tokens_x_2d.size()
+        token_type_ids_2d = torch.LongTensor(token_type_ids_l).to(self.device)
+
+        batch_size, seq_length = tokens_x_2d[:,1:].size()
         self.PreModel.eval()
         with torch.no_grad():
-            emb, _ = self.PreModel(tokens_x_2d)
+            emb, _ = self.PreModel(tokens_x_2d,token_type_ids=token_type_ids_2d)
+
+        ## [CLS] for IsQA  [batch_size, hidden_size]
+        cls_emb = emb[:,0,:]
+        ## [batch_size, 2]
+        IsQA_logits = self.fc2(cls_emb)
+        ## [batch_size, 1]
+        IsQA_prediction = IsQA_logits.argmax(dim=-1)
+
         # CRF mask
         mask = np.ones(shape=[batch_size, seq_length], dtype=np.uint8)
         mask = torch.ByteTensor(mask).to(self.device)
         # [batch_size, seq_len, 4]
-        crf_logits = self.CRF_fc1(emb)
-        _, prediction = self.CRF.forward(feats=crf_logits, mask=mask)
-        return prediction.to("cpu")
+        crf_logits = self.CRF_fc1(emb[:,1:,:])
+        _, CRFprediction = self.CRF.forward(feats=crf_logits, mask=mask)
+
+        return IsQA_prediction.to("cpu"), CRFprediction.to("cpu")
